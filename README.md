3b6f6895a938797acaee8cbdc797fdb40bfbc2cd
George Prokopakis <gprok60@gmail.com>
2016-07-22 12:30:20 +0300

    All IR sensor added and enables. Both servo mototrs added and enabled

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Simple Line-Following Robot
+
+Up to this point the robot reads from 5 IR sensors and is able to control the 2 servo motors.
+
+Connection and cabling diagrams to be added soon.
--- slfr/slfr.ino
+++ slfr/slfr.ino
@@ -1,21 +1,29 @@
+int duration;
+int distance;
+int sensorPin = 7;
+
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  
-
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  int BLACK_EST = 600;
-
-  if(sensorValue > BLACK_EST) {
-     digitalWrite(13, HIGH);
-  }
-  else {
+  // put your main code here, to run repeatedly:
+  pinMode(sensorPin, OUTPUT);
+  digitalWrite(sensorPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(sensorPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensorPin, LOW);
+  pinMode(sensorPin, INPUT);
+  duration = pulseIn(sensorPin, HIGH);
+  distance = duration/58;
+  if(distance < 25) {
+    digitalWrite(13, HIGH); // Turn on LED
+    delay(1000);
     digitalWrite(13, LOW);
   }
-  
-  Serial.println(sensorValue);
-  delay(1);
+  Serial.println(distance);
+  delay(100);
 }
